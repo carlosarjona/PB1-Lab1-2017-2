d64ce009cef647d1a33c17ecab8c10858f0ba8c3
Carlos ' Arjona <eduardo.arjona@hotmail.com>
2017-09-25 20:29:34 -0300

    Add project files.

---
+++ prova/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(prova.Startup))]
+namespace prova
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
